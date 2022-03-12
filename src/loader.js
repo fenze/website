0495e330a6f6faf334faf1779504f71447def004
fenze <contact@fenze.dev>
2022-03-12 10:16:37 +0100

    style: clean up

--- src/loader.js
+++ src/loader.js
@@ -1,8 +1,5 @@
 window.addEventListener("load", () => {
-	document.body.insertAdjacentHTML('afterend','
-		<div id="loader">fenze</div>
-	')
 	setTimeout(() => {
 		document.querySelector("#loader").remove()
-	}, 400);
+	}, 10000);
 })
