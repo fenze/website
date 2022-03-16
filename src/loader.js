9452b6e6f7b13218461569955c7c88326cc9da49
fenze <contact@fenze.dev>
2022-03-16 12:56:40 +0100

    search: improve search

--- src/loader.js
+++ src/loader.js
@@ -8,7 +8,7 @@ mbtn.click(() => {
     mbtn.addClass("fa-xmark")
     nav.show()
     nav.css("display: flex")
-    $("nav > input").focus()
+    $("#box > input").focus()
   } else {
     mbtn.removeClass("fa-xmark")
     mbtn.addClass("fa-bars")
@@ -17,6 +17,20 @@ mbtn.click(() => {
 })
 
 $(document).on("keyup", e => {
+  console.log(e.keyCode)
   if (e.keyCode === 27 || e.keyCode === 191)
     mbtn.click()
+})
+
+$("#box > input").on("keyup", e => {
+  input = document.querySelector("#box > input").value.toUpperCase()
+  menu = document.querySelectorAll(".search-element")
+
+  for (element of menu)
+    if (element.innerHTML.toUpperCase().indexOf(input) == -1) {
+      element.style.display = "none"
+    }
+    else {
+      element.style.display = "flex"
+    }
 })
