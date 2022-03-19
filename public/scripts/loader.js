43c4889f9c19022429922e271e091583a66d443f
fenze <contact@fenze.dev>
2022-03-19 16:24:55 +0100

    add offer section

--- public/scripts/loader.js
+++ public/scripts/loader.js
@@ -1,39 +1,48 @@
-$(setTimeout(() => $(".loader").remove(), 400))
+$(setTimeout(() => $('.loader').remove(), 400))
 
-mbtn = $(".m-btn")
-nav = $("nav")
+const search = () => {
+  input = document.querySelector('#box > input').value.toUpperCase()
+  menu = document.querySelectorAll('.search-element')
+
+  for (element of menu)
+    if (element.innerHTML.toUpperCase().indexOf(input) == -1) {
+      element.style.display = 'none'
+    }
+    else {
+      element.style.display = 'flex'
+    }
+}
+
+mbtn = $('.m-btn')
+nav = $('nav')
 mbtn.click(() => {
-  if (nav.css("display") == "none") {
-    mbtn.removeClass("fa-bars")
-    mbtn.addClass("fa-xmark")
+  if (nav.css('display') == "none") {
+    mbtn.removeClass('fa-bars')
+    mbtn.addClass('fa-xmark')
+    mbtn.css('color', '#FEC8A7')
     nav.css('display', 'flex')
-    $("#box > input").focus()
+    input=$('#box > input').val('')
+    input.focus()
+    search()
   } else {
-    mbtn.removeClass("fa-xmark")
-    mbtn.addClass("fa-bars")
+    mbtn.removeClass('fa-xmark')
+    mbtn.addClass('fa-bars')
+    mbtn.css('color', '#CBC7FC')
     nav.hide()
   }
 })
 
-$(document).on("keyup", e => {
+
+$(document).on('keyup', e => {
   if (e.keyCode === 27 || e.keyCode === 191)
     mbtn.click()
 
   if (e.keyCode === 13)
-      for (entry of document.querySelectorAll(".search-element"))
-        if (entry.style.display != "none")
-          window.open(entry.getAttribute("href"), name='_self')
+      for (entry of document.querySelectorAll('.search-element'))
+        if (entry.style.display != 'none') {
+          mbtn.click()
+          window.open(entry.getAttribute('href'), name='_self')
+        }
 })
 
-$("#box > input").on("keyup", e => {
-  input = document.querySelector("#box > input").value.toUpperCase()
-  menu = document.querySelectorAll(".search-element")
-
-  for (element of menu)
-    if (element.innerHTML.toUpperCase().indexOf(input) == -1) {
-      element.style.display = "none"
-    }
-    else {
-      element.style.display = "flex"
-    }
-})
+$('#box > input').on("keyup", () => search())
